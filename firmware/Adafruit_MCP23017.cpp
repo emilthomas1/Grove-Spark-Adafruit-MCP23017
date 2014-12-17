6b35fcdfd33fdee879a4fc2fc3a7b04c533f145c
Louis DeScioli <louis.descioli@gmail.com>
2014-12-16 22:38:15 -0500

    Library rewrite
    Updated method names, added a couple methods, added comments

--- firmware/Adafruit_MCP23017.cpp
+++ firmware/Adafruit_MCP23017.cpp
@@ -1,288 +1,187 @@
 /*************************************************** 
- This is a library for the MCP23017 i2c port expander
+ This is a library for the Adafruit MCP23017 I2C port expander
 
- These displays use I2C to communicate, 2 pins are required to
- interface
- Adafruit invests time and resources providing this open source code,
- please support Adafruit and open-source hardware by purchasing
- products from Adafruit!
+ Adafruit invests time and resources providing this open source code, please 
+ support Adafruit and open-source hardware by purchasing products from 
+ Adafruit!
 
  Written by Limor Fried/Ladyada for Adafruit Industries.
  BSD license, all text above must be included in any redistribution
  
  Adapted for Spark Core by Paul Kourany, Sept 3, 2014
+ Extended for Grove Labs by Louis DeScioli, Dec 16 2014
  ****************************************************/
 
-#if defined (SPARK)
-#include "Adafruit_MCP23017.h"
-#else
-#include <Wire.h>
-#ifdef __AVR__
-#include <avr/pgmspace.h>
-#endif
 #include "Adafruit_MCP23017.h"
 
-#if ARDUINO >= 100
-#include "Arduino.h"
-#else
-#include "WProgram.h"
-#endif
-#endif //Spark
-
-// minihelper to keep Arduino backward compatibility
-static inline void wiresend(uint8_t x) {
-#if (ARDUINO >= 100) || defined (SPARK)
-	Wire.write((uint8_t) x);
-#else
-	Wire.send(x);
-#endif
-}
-
-static inline uint8_t wirerecv(void) {
-#if (ARDUINO >= 100) || defined (SPARK)
-	return Wire.read();
-#else
-	return Wire.receive();
-#endif
-}
-
 /**
- * Bit number associated to a give Pin
+ * Initializes the MCP23017 with an I2C address.
+ * The address must match what the address bits are wired to. If no parameters
+ * are given, it will be assumed that the address pins are 0 and the default
+ * address as specified in the header file will be used.
+ * @param addr    the I2C address of the chip. Should be 0->7, only 3 bits
+ * @param debug   whether or not to print debugging statements to Serial port
  */
-uint8_t Adafruit_MCP23017::bitForPin(uint8_t pin){
-	return pin%8;
+Adafruit_MCP23017::Adafruit_MCP23017(uint8_t addr, bool debug) {
+  i2caddr = addr;
+  debugging = debug;
 }
 
 /**
- * Register address, port dependent, for a given PIN
- */
-uint8_t Adafruit_MCP23017::regForPin(uint8_t pin, uint8_t portAaddr, uint8_t portBaddr){
-	return(pin<8) ?portAaddr:portBaddr;
-}
-
-/**
- * Reads a given register
- */
-uint8_t Adafruit_MCP23017::readRegister(uint8_t addr){
-	// read the current GPINTEN
-	Wire.beginTransmission(MCP23017_ADDRESS | i2caddr);
-	wiresend(addr);
-	Wire.endTransmission();
-	Wire.requestFrom(MCP23017_ADDRESS | i2caddr, 1);
-	return wirerecv();
-}
-
-
-/**
- * Writes a given register
- */
-void Adafruit_MCP23017::writeRegister(uint8_t regAddr, uint8_t regValue){
-	// Write the register
-	Wire.beginTransmission(MCP23017_ADDRESS | i2caddr);
-	wiresend(regAddr);
-	wiresend(regValue);
-	Wire.endTransmission();
-}
-
-
-/**
- * Helper to update a single bit of an A/B register.
- * - Reads the current register value
- * - Writes the new register value
- */
-void Adafruit_MCP23017::updateRegisterBit(uint8_t pin, uint8_t pValue, uint8_t portAaddr, uint8_t portBaddr) {
-	uint8_t regValue;
-	uint8_t regAddr=regForPin(pin,portAaddr,portBaddr);
-	uint8_t bit=bitForPin(pin);
-	regValue = readRegister(regAddr);
-
-	// set the value for the particular bit
-	bitWrite(regValue,bit,pValue);
-
-	writeRegister(regAddr,regValue);
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * Initializes the MCP23017 given its HW selected address, see datasheet for Address selection.
- */
-void Adafruit_MCP23017::begin(uint8_t addr) {
-	if (addr > 7) {
-		addr = 7;
-	}
-	i2caddr = addr;
-
-	Wire.begin();
-
-	// set defaults!
-	// all inputs on port A and B
-	writeRegister(MCP23017_IODIRA,0xff);
-	writeRegister(MCP23017_IODIRB,0xff);
-}
-
-/**
- * Initializes the default MCP23017, with 000 for the configurable part of the address
+ * Join the I2C bus as a master
+ * Not necessarily needed if Wire.begin() is called elsewhere
  */
 void Adafruit_MCP23017::begin(void) {
-	begin(0);
+  Wire.begin();
 }
 
 /**
- * Sets the pin mode to either INPUT or OUTPUT
+ * Set the mode of a GPIO pin
+ * @param bank  The GPIO bank, either A or B. If not 'a' assumed to be 'b'
+ * @param pin   The GPIO pin number
+ * @param mode  Either INPUT (1) or OUTPUT (0)
  */
-void Adafruit_MCP23017::pinMode(uint8_t p, uint8_t d) {
-	updateRegisterBit(p,(d==INPUT),MCP23017_IODIRA,MCP23017_IODIRB);
+void Adafruit_MCP23017::setGPIOPinMode(char bank, uint8_t pin, uint8_t mode) {
+  tolower(bank) == 'a' ? setBit(MCP23017_IODIRA, pin, mode) : setBit(MCP23017_IODIRB, pin, mode);
 }
 
 /**
- * Reads all 16 pins (port A and B) into a single 16 bits variable.
+ * Enable or disable an internal pullup resistor for a given GPIO pin
+ * @param bank  The GPIO bank, either A or B. If not 'a' assumed to be 'b'
+ * @param pin   The GPIO pin number, should be 0->7
+ * @param mode  The state of the pinup, should be 0 or 1
  */
-uint16_t Adafruit_MCP23017::readGPIOAB() {
-	uint16_t ba = 0;
-	uint8_t a;
-
-	// read the current GPIO output latches
-	Wire.beginTransmission(MCP23017_ADDRESS | i2caddr);
-	wiresend(MCP23017_GPIOA);
-	Wire.endTransmission();
-
-	Wire.requestFrom(MCP23017_ADDRESS | i2caddr, 2);
-	a = wirerecv();
-	ba = wirerecv();
-	ba <<= 8;
-	ba |= a;
-
-	return ba;
+void Adafruit_MCP23017::setGPIOPullUp(char bank, uint8_t pin, uint8_t mode) {
+  tolower(bank) == 'a' ? setBit(MCP23017_GPPUA, pin, mode) : setBit(MCP23017_GPPUB, pin, mode);
 }
 
 /**
- * Read a single port, A or B, and return its current 8 bit value.
- * Parameter b should be 0 for GPIOA, and 1 for GPIOB.
+ * Get the value of a single GPIO pin for a given bank
+ * @param  bank  The GPIO bank, either A or B. If not 'a' assumed to be 'b'
+ * @param  pin   The pin to read, 0->7
+ * @return       The digital value of the pin, either 0 or 1
  */
-uint8_t Adafruit_MCP23017::readGPIO(uint8_t b) {
-
-	// read the current GPIO output latches
-	Wire.beginTransmission(MCP23017_ADDRESS | i2caddr);
-	if (b == 0)
-		wiresend(MCP23017_GPIOA);
-	else {
-		wiresend(MCP23017_GPIOB);
-	}
-	Wire.endTransmission();
-
-	Wire.requestFrom(MCP23017_ADDRESS | i2caddr, 1);
-	return wirerecv();
+uint8_t Adafruit_MCP23017::readGPIOPin(char bank, uint8_t pin) {
+  return bitRead(readGPIOBank(bank), pin);
 }
 
 /**
- * Writes all the pins in one go. This method is very useful if you are implementing a multiplexed matrix and want to get a decent refresh rate.
+ * Get the values of a bank of GPIO pins
+ * @param  bank  The GPIO bank, either A or B. If not 'a' assumed to be 'b'
+ * @return  The values of the 8 GPIO pins in the given bank, as an 8-bit int
  */
-void Adafruit_MCP23017::writeGPIOAB(uint16_t ba) {
-	Wire.beginTransmission(MCP23017_ADDRESS | i2caddr);
-	wiresend(MCP23017_GPIOA);
-	wiresend(ba & 0xFF);
-	wiresend(ba >> 8);
-	Wire.endTransmission();
-}
-
-void Adafruit_MCP23017::digitalWrite(uint8_t pin, uint8_t d) {
-	uint8_t gpio;
-	uint8_t bit=bitForPin(pin);
-
-
-	// read the current GPIO output latches
-	uint8_t regAddr=regForPin(pin,MCP23017_OLATA,MCP23017_OLATB);
-	gpio = readRegister(regAddr);
-
-	// set the pin and direction
-	bitWrite(gpio,bit,d);
-
-	// write the new GPIO
-	regAddr=regForPin(pin,MCP23017_GPIOA,MCP23017_GPIOB);
-	writeRegister(regAddr,gpio);
-}
-
-void Adafruit_MCP23017::pullUp(uint8_t p, uint8_t d) {
-	updateRegisterBit(p,d,MCP23017_GPPUA,MCP23017_GPPUB);
-}
-
-uint8_t Adafruit_MCP23017::digitalRead(uint8_t pin) {
-	uint8_t bit=bitForPin(pin);
-	uint8_t regAddr=regForPin(pin,MCP23017_GPIOA,MCP23017_GPIOB);
-	return (readRegister(regAddr) >> bit) & 0x1;
+uint8_t Adafruit_MCP23017::readGPIOBank(char bank) {
+  return tolower(bank) == 'a' ? readRegister(MCP23017_GPIOA) : readRegister(MCP23017_GPIOB);
 }
 
 /**
- * Configures the interrupt system. both port A and B are assigned the same configuration.
- * Mirroring will OR both INTA and INTB pins.
- * Opendrain will set the INT pin to value or open drain.
- * polarity will set LOW or HIGH on interrupt.
- * Default values after Power On Reset are: (false,flase, LOW)
- * If you are connecting the INTA/B pin to arduino 2/3, you should configure the interupt handling as FALLING with
- * the default configuration.
+ * Get the values of all 16 GPIO pins
+ * Make sure that the GPIO pins have been setup as inputs!
+ * @return  The 16 values of the GPIO pins as an unsigned 16-bit int.
+ *          8-MSB are bank B, 8-LSB are bank A
  */
-void Adafruit_MCP23017::setupInterrupts(uint8_t mirroring, uint8_t openDrain, uint8_t polarity){
-	// configure the port A
-	uint8_t ioconfValue=readRegister(MCP23017_IOCONA);
-	bitWrite(ioconfValue,6,mirroring);
-	bitWrite(ioconfValue,2,openDrain);
-	bitWrite(ioconfValue,1,polarity);
-	writeRegister(MCP23017_IOCONA,ioconfValue);
-
-	// Configure the port B
-	ioconfValue=readRegister(MCP23017_IOCONB);
-	bitWrite(ioconfValue,6,mirroring);
-	bitWrite(ioconfValue,2,openDrain);
-	bitWrite(ioconfValue,1,polarity);
-	writeRegister(MCP23017_IOCONB,ioconfValue);
+uint16_t Adafruit_MCP23017::readAllGPIO() {
+  uint16_t result = 0;
+  uint8_t resultA = readRegister(MCP23017_GPIOA);
+  uint8_t resultB = readRegister(MCP23017_GPIOB);
+  result |= resultB;
+  result <<= 8;
+  result |= resultA;
+  return result;
 }
 
 /**
- * Set's up a pin for interrupt. uses arduino MODEs: CHANGE, FALLING, RISING.
- *
- * Note that the interrupt condition finishes when you read the information about the port / value
- * that caused the interrupt or you read the port itself. Check the datasheet can be confusing.
- *
+ * Write a value out of a GPIO pin for a given bank, leaving the others untouched
+ * @param bank  A GPIO bank, either A or B. If not 'a' will be assumed to be 'b'
+ * @param pin   The specific pin outputting the given value
+ * @param value The digital output value for the pin, either 0 or 1
  */
-void Adafruit_MCP23017::setupInterruptPin(uint8_t pin, uint8_t mode) {
-
-	// set the pin interrupt control (0 means change, 1 means compare against given value);
-	updateRegisterBit(pin,(mode!=CHANGE),MCP23017_INTCONA,MCP23017_INTCONB);
-	// if the mode is not CHANGE, we need to set up a default value, different value triggers interrupt
-
-	// In a RISING interrupt the default value is 0, interrupt is triggered when the pin goes to 1.
-	// In a FALLING interrupt the default value is 1, interrupt is triggered when pin goes to 0.
-	updateRegisterBit(pin,(mode==FALLING),MCP23017_DEFVALA,MCP23017_DEFVALB);
-
-	// enable the pin for interrupt
-	updateRegisterBit(pin,HIGH,MCP23017_GPINTENA,MCP23017_GPINTENB);
-
+void Adafruit_MCP23017::writeGPIOPin(char bank, uint8_t pin, uint8_t value) {
+  bank == 'a' ? setBit(MCP23017_GPIOA, pin, value) : setBit(MCP23017_GPIOB, pin, value);
 }
 
-uint8_t Adafruit_MCP23017::getLastInterruptPin(){
-	uint8_t intf;
-
-	// try port A
-	intf=readRegister(MCP23017_INTFA);
-	for(int i=0;i<8;i++) if (bitRead(intf,i)) return i;
-
-	// try port B
-	intf=readRegister(MCP23017_INTFB);
-	for(int i=0;i<8;i++) if (bitRead(intf,i)) return i+8;
-
-	return MCP23017_INT_ERR;
-
-}
-uint8_t Adafruit_MCP23017::getLastInterruptPinValue(){
-	uint8_t intPin=getLastInterruptPin();
-	if(intPin!=MCP23017_INT_ERR){
-		uint8_t intcapreg=regForPin(intPin,MCP23017_INTCAPA,MCP23017_INTCAPB);
-		uint8_t bit=bitForPin(intPin);
-		return (readRegister(intcapreg)>>bit) & (0x01);
-	}
-
-	return MCP23017_INT_ERR;
+/**
+ * Writes to all of the GPIO in one go. This method is very useful if you are
+ * implementing a multiplexed matrix and want to get a decent refresh rate.
+ * Make sure that the GPIO pins have been setup as outputs!
+ * @param value  The 16 bit value to be written. 
+ *               The 8-LSB go to bank A, 8-MSB go to bank B
+ */
+void Adafruit_MCP23017::writeAllGPIO(uint16_t value) {
+  uint8_t aValue = (value & 0xff);
+  uint8_t bValue = (( value >> 8 ) & 0xff);
+  writeRegister(MCP23017_GPIOA, aValue);
+  writeRegister(MCP23017_GPIOB, bValue);
 }
 
+/**
+ * Write a byte out for a given bank of GPIO pins
+ * @param bank  The bank of GPIO pins. If not 'a' will be treated as 'b'
+ * @param value The 8-bit value to be output
+ */
+void Adafruit_MCP23017::writeGPIOBank(char bank, uint8_t value) {
+  tolower(bank) == 'a' ? writeRegister(MCP23017_GPIOA, value) : writeRegister(MCP23017_GPIOB, value);
+}
 
+/**
+ * Configure some parts of the chip. Port A and B are assigned the same configuration.
+ * @param mirroring  OR the INTA and INTB pins. POR value is false
+ * @param openDrain  Set the INT pin to value or open drain. POR value is false
+ * @param polarity   Set LOW or HIGH on interrupt. POR value is LOW
+ */
+void Adafruit_MCP23017::configure(uint8_t mirroring, uint8_t openDrain, uint8_t polarity){
+  // configure the port A
+  uint8_t ioconfValue = readRegister(MCP23017_IOCONA);
+  bitWrite(ioconfValue, 6, mirroring);
+  bitWrite(ioconfValue, 2, openDrain);
+  bitWrite(ioconfValue, 1, polarity);
+  writeRegister(MCP23017_IOCONA, ioconfValue);
+
+  // Configure the port B
+  ioconfValue = readRegister(MCP23017_IOCONB);
+  bitWrite(ioconfValue, 6, mirroring);
+  bitWrite(ioconfValue, 2, openDrain);
+  bitWrite(ioconfValue, 1, polarity);
+  writeRegister(MCP23017_IOCONB, ioconfValue);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Private Methods
+/////////////////////////////////////////////////////////////////////////////// 
+
+/**
+ * Read the byte stored in a register
+ * @param  addr  The address of the register on the MCP23017 to read
+ * @return       The 8-bit value stored in the register
+ */
+uint8_t Adafruit_MCP23017::readRegister(uint8_t addr){
+  Wire.beginTransmission(i2caddr);
+  Wire.write(addr);
+  Wire.endTransmission();
+  Wire.requestFrom(i2caddr, 1);
+  return Wire.read();
+}
+
+/**
+ * Write a byte to a given register
+ * @param addr   The address of the register on the MCP23017 to write to
+ * @param value  The 8-bit value to write
+ */
+void Adafruit_MCP23017::writeRegister(uint8_t addr, uint8_t value){
+  Wire.beginTransmission(i2caddr);
+  Wire.write(addr);
+  Wire.write(value);
+  Wire.endTransmission();
+}
+
+/**
+ * Edit a single bit of a given register
+ * @param regAddr The address of the register to be written to
+ * @param bit     The bit position to be set
+ * @param value   The value to be set at the bit position (0 or 1)
+ */
+void Adafruit_MCP23017::setBit(uint8_t regAddr, uint8_t bit, uint8_t value) {
+  uint8_t number = readRegister(regAddr);   // Read out the current value
+  bitWrite(number, bit, value);           // write the value at the bit position
+  writeRegister(regAddr, number);     // write the byte back to the register
+}
--- firmware/Adafruit_MCP23017.h
+++ firmware/Adafruit_MCP23017.h
@@ -14,81 +14,66 @@
 #ifndef _Adafruit_MCP23017_H_
 #define _Adafruit_MCP23017_H_
 
-#if defined (SPARK)
 #include "application.h"
 
-#define bitRead(value, bit) (((value) >> (bit)) & 0x01)
-#define bitSet(value, bit) ((value) |= (1UL << (bit)))
-#define bitClear(value, bit) ((value) &= ~(1UL << (bit)))
-#define bitWrite(value, bit, bitvalue) (bitvalue ? bitSet(value, bit) : bitClear(value, bit))
-
-#endif
-
-// Don't forget the Wire library
-class Adafruit_MCP23017 {
-public:
-  void begin(uint8_t addr);
-  void begin(void);
-
-  void pinMode(uint8_t p, uint8_t d);
-  void digitalWrite(uint8_t p, uint8_t d);
-  void pullUp(uint8_t p, uint8_t d);
-  uint8_t digitalRead(uint8_t p);
-
-  void writeGPIOAB(uint16_t);
-  uint16_t readGPIOAB();
-  uint8_t readGPIO(uint8_t b);
-
-  void setupInterrupts(uint8_t mirroring, uint8_t open, uint8_t polarity);
-  void setupInterruptPin(uint8_t p, uint8_t mode);
-  uint8_t getLastInterruptPin();
-  uint8_t getLastInterruptPinValue();
-
- private:
-  uint8_t i2caddr;
-
-  uint8_t bitForPin(uint8_t pin);
-  uint8_t regForPin(uint8_t pin, uint8_t portAaddr, uint8_t portBaddr);
-
-  uint8_t readRegister(uint8_t addr);
-  void writeRegister(uint8_t addr, uint8_t value);
-
-  /**
-   * Utility private method to update a register associated with a pin (whether port A/B)
-   * reads its value, updates the particular bit, and writes its value.
-   */
-  void updateRegisterBit(uint8_t p, uint8_t pValue, uint8_t portAaddr, uint8_t portBaddr);
-
-};
-
-#define MCP23017_ADDRESS 0x20
-
-// registers
+// Register address -- these are only good if IOCON.BANK is set to 0
 #define MCP23017_IODIRA 0x00
-#define MCP23017_IPOLA 0x02
-#define MCP23017_GPINTENA 0x04
-#define MCP23017_DEFVALA 0x06
-#define MCP23017_INTCONA 0x08
-#define MCP23017_IOCONA 0x0A
-#define MCP23017_GPPUA 0x0C
-#define MCP23017_INTFA 0x0E
-#define MCP23017_INTCAPA 0x10
-#define MCP23017_GPIOA 0x12
-#define MCP23017_OLATA 0x14
-
-
 #define MCP23017_IODIRB 0x01
+#define MCP23017_IPOLA 0x02
 #define MCP23017_IPOLB 0x03
+#define MCP23017_GPINTENA 0x04
 #define MCP23017_GPINTENB 0x05
+#define MCP23017_DEFVALA 0x06
 #define MCP23017_DEFVALB 0x07
+#define MCP23017_INTCONA 0x08
 #define MCP23017_INTCONB 0x09
-#define MCP23017_IOCONB 0x0B
+#define MCP23017_IOCONA 0x0A  // The differentation between IOCON A & B
+#define MCP23017_IOCONB 0x0B  // doesn't actually mean anything. See datasheet
+#define MCP23017_GPPUA 0x0C
 #define MCP23017_GPPUB 0x0D
+#define MCP23017_INTFA 0x0E
 #define MCP23017_INTFB 0x0F
+#define MCP23017_INTCAPA 0x10
 #define MCP23017_INTCAPB 0x11
+#define MCP23017_GPIOA 0x12
 #define MCP23017_GPIOB 0x13
+#define MCP23017_OLATA 0x14
 #define MCP23017_OLATB 0x15
 
 #define MCP23017_INT_ERR 255
+
+// reads the bit of a number
+#define bitRead(number, bit) ( (number >> bit) & 0x01 )
+// writes a 1 to a bit of a number
+#define bitSet(number, bit) ( number |= (1UL << bit) )
+// writes a 0 to a bit of a number
+#define bitClear(number, bit) ( number &= ~(1UL << bit) )
+// writes a specified value (0 or 1) to a bit of a number
+#define bitWrite(number, bit, value) ( value ? bitSet(number, bit) : bitClear(number, bit) )
+
+
+class Adafruit_MCP23017 {
+ public:
+  Adafruit_MCP23017(uint8_t addr = 0x20, bool debug = false);
+  void begin(void);
+  void setGPIOPinMode(char bank, uint8_t pin, uint8_t mode);
+  void setGPIOPullUp(char bank, uint8_t pin, uint8_t mode);
+  uint8_t readGPIOPin(char bank, uint8_t pin);
+  uint8_t readGPIOBank(char bank);
+  uint16_t readAllGPIO(void);
+  void writeGPIOPin(char bank, uint8_t pin, uint8_t value);
+  void writeGPIOBank(char bank, uint8_t value);
+  void writeAllGPIO(uint16_t value);
+  void configure(uint8_t mirroring, uint8_t open, uint8_t polarity);
+
+ private:
+  uint8_t i2caddr;
+  bool debugging;
+
+  uint8_t readRegister(uint8_t addr);
+  void writeRegister(uint8_t addr, uint8_t value);
+  void setBit(uint8_t regAddr, uint8_t bit, uint8_t value);
+
+};
 
 #endif
